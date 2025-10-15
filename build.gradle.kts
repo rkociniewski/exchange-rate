6660d80373c5fac17af5e691004d42ae649d9200
Rafal Kociniewski <rafal.kociniewski@gmail.com>
2025-10-15 20:36:53 +0200

    ci: qodana

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ group = "rk.powermilk"
 /**
  * project version
  */
-version = "1.1.3"
+version = "1.1.6"
 
 val javaVersion: JavaVersion = JavaVersion.VERSION_21
 val jvmTargetVersion = JvmTarget.JVM_21.target
